0cd45292ae7d36334c1e4994daa5adf8ee138bf3
Shuting Guan <shuting.guan@teachforamerica.org>
2012-11-07 13:59:11 -0500

    change 1

--- src/org/tfa/test/helloworld/HelloWorld.java
+++ src/org/tfa/test/helloworld/HelloWorld.java
@@ -6,8 +6,11 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(hello("cat"));
+	}
+	
+	public static String hello(String name){
+		return "hello "+name;
 	}
 
 }
